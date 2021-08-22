788b3470f971619e4b784738a4438ed105c35e90
ashishkeshri514 <ashish.keshri.514@gmail.com>
2021-08-23 00:20:47 +0530

    Transpile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Content from './components/Content';
 import Header from './components/Header';
-import './App.scss';
+import './App.css';
 function App() {
     return (
         <div>
